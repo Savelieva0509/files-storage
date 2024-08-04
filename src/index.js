84c33342463f6674d738fadfc48c05462c1d14c3
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-05 02:02:26 +0300

    del packages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './components/App/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/files-storage">
