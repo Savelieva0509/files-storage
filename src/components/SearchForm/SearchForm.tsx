4a8cabd5b823e2d1683b46cbbc8a1d9d37069ed6
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-05 00:20:45 +0300

    add style searchBtn

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { setSearchQuery } from '../../redux/search-slice';
 import css from './SearchForm.module.scss';
 
@@ -16,6 +15,24 @@ const SearchForm = () => {
     dispatch(setSearchQuery(query));
   };
 
+  const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const button = e.currentTarget;
+    const rect = button.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    button.style.setProperty('--left', `${x}px`);
+    button.style.setProperty('--top', `${y}px`);
+    button.classList.add(css.active);
+  };
+
+  const handleMouseUp = () => {
+    const button = document.querySelector(`.${css.searchButton}`);
+    if (button) {
+      button.classList.remove(css.active);
+    }
+  };
+
   return (
     <form className={css.searchForm}>
       <input
@@ -25,8 +42,14 @@ const SearchForm = () => {
         onChange={handleChange}
         className={css.searchInput}
       />
-      <button className={css.searchButton} onClick={handleSearch}>
-        Search
+      <button
+        className={css.searchButton}
+        type="button"
+        onClick={handleSearch}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
+        <span>Search</span>
       </button>
     </form>
   );
